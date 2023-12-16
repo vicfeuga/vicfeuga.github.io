122a27f841164cf1b44bd1077647940dfeb4ffcd
Victor <50053627+vicfeuga@users.noreply.github.com>
2023-12-16 22:26:12 +0000

    js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ if (document.getElementById('my-work-link')) {
 }
 
 const typedTextSpan = document.querySelector(".typed-text");
-const cursorSpan = document.querySelector(".cursor");
 
 const textArray = ["data scientist", "ml enthousiaste", "data analyst"];
 const typingDelay = 200;
@@ -16,26 +15,22 @@ let charIndex = 0;
 
 function type() {
   if (charIndex < textArray[textArrayIndex].length) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
     typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
     charIndex++;
     setTimeout(type, typingDelay);
   } 
   else {
-    cursorSpan.classList.remove("typing");
   	setTimeout(erase, newTextDelay);
   }
 }
 
 function erase() {
 	if (charIndex > 0) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
     typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
     charIndex--;
     setTimeout(erase, erasingDelay);
   } 
   else {
-    cursorSpan.classList.remove("typing");
     textArrayIndex++;
     if(textArrayIndex>=textArray.length) textArrayIndex=0;
     setTimeout(type, typingDelay + 1100);
