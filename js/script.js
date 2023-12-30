de831cdbab7d2fe48350e27e352c16ed9a6ba24a
vicfeuga <vicfeuga@orange.fr>
2023-12-30 19:48:59 +0100

    few changes in display

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@ if (document.getElementById('my-work-link')) {
 
 const typedTextSpan = document.querySelector(".typed-text");
 
-const textArray = ["data scientist", "ml enthousiaste", "data analyst"];
+const textArray = ["data scientist,", "ml enthousiaste,", "data analyst,"];
 const typingDelay = 130;
 const erasingDelay = 100;
-const newTextDelay = 1500; // Delay between current and next text
+const newTextDelay = 1200; // Delay between current and next text
 let textArrayIndex = 0;
 let charIndex = 0;
 
