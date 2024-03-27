cbd046e8e966b027ebfbac72dca802cc8cbed78f
Victor <50053627+vicfeuga@users.noreply.github.com>
2024-03-27 22:56:11 +0100

    push

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
 if (document.getElementById('my-work-link')) {
   document.getElementById('my-work-link').addEventListener('click', () => {
-    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
+    document.getElementById('experiences-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+const typedTextSpan = document.querySelector(".typed-text");
+
+const textArray = ["data scientist,", "ml enthousiaste,", "data analyst,"];
+const typingDelay = 130;
+const erasingDelay = 100;
+const newTextDelay = 1200; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
